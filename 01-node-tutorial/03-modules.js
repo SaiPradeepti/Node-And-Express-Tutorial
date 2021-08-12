989e245e516e0d9f445cff244c8534b0de5ac60d
Sai Pradeepti <saipradeepti14@gmail.com>
2021-08-12 18:13:21 +0530

    Alternative export methods

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -1,9 +1,14 @@
 //modules
 const {john,peter} = require('./04-names')
 const {sayHi,factorial} = require('./05-utils')
+const data = require('./06-alternative-export')
+const {groceryItems,singleStudent} = require('./06-alternative-export')
 
 sayHi('Susan')
 sayHi(john)
 sayHi(peter)
 
 factorial(5)
+
+console.log(data)
+console.log(groceryItems,singleStudent)
---
+++ 01-node-tutorial/06-alternative-export.js
@@ -0,0 +1,7 @@
+module.exports.groceryItems = ['wheat flour','brown bread','butter','jam'];
+
+const student = {
+    name: 'Sai Pradeepti'
+}
+
+module.exports.singleStudent = student;
