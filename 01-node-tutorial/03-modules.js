bf9ece8bd8623827ab6d7cc5c6245cf72be0b0b2
Sai Pradeepti <saipradeepti14@gmail.com>
2021-08-12 17:48:54 +0530

    Multiple Exports

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -1,7 +1,9 @@
 //modules
 const {john,peter} = require('./04-names')
-const sayHi = require('./05-utils')
+const {sayHi,factorial} = require('./05-utils')
 
 sayHi('Susan')
 sayHi(john)
 sayHi(peter)
+
+factorial(5)
--- 01-node-tutorial/05-utils.js
+++ 01-node-tutorial/05-utils.js
@@ -2,4 +2,12 @@ const sayHi = name => {
     console.log(`Hi! ${name}`)
 }
 
-module.exports = sayHi;
+const factorial = (num) => {
+    let factorial = 1;
+    for(let i=num; i>=1; i--){
+        factorial *= i
+    }
+    console.log(factorial)
+}
+
+module.exports = {sayHi,factorial};
